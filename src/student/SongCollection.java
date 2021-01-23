856d42a943a060f5e8f36ce10a463784caed8709
Caleb Tracey <caleb.tracey@gmail.com>
2021-01-22 20:03:05 -0500

    Initial setup for Data Structures project

---
+++ src/student/SongCollection.java
@@ -0,0 +1,65 @@
+package student;
+
+import java.util.stream.Stream;
+
+/*
+ * 
+ * 8.2016 - Anne Applin - formatting and JavaDoc skeletons added   
+ * 2015 - Prof. Bob Boothe - Starting code and main for testing  
+ ************************************************************************
+ * SongCollection.java 
+ * Read the specified data file and build an array of songs.
+ */
+/**
+ *
+ * @author boothe
+ */
+public class SongCollection {
+
+    private Song[] songs;
+
+    /**
+     * Note: in any other language, reading input inside a class is simply not
+     * done!! No I/O inside classes because you would normally provide
+     * precompiled classes and I/O is OS and Machine dependent and therefore not
+     * portable. Java runs on a virtual machine that IS portable. So this is
+     * permissable because we are programming in Java.
+     *
+     * @param filename The path and filename to the datafile that we are using
+     * must be set in the Project Properties as an argument.
+     */
+    public SongCollection(String filename) {
+
+	// use a try catch block
+        // read in the song file and build the songs array
+        // you must use a StringBuilder to read in the lyrics!
+        // you must add the line feed at the end of each lyric line.
+        
+        
+        // sort the songs array using Array.sort (see the Java API)
+    }
+ 
+    /**
+     * this is used as the data source for building other data structures
+     * @return the songs array
+     */
+    public Song[] getAllSongs() {
+        return songs;
+    }
+ 
+    /**
+     * unit testing method
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("usage: prog songfile");
+            return;
+        }
+
+        SongCollection sc = new SongCollection(args[0]);
+
+        // todo: show song count 
+        Stream.of(sc).limit(10).forEach(System.out::println);
+    }
+}
