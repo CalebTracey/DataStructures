32a3e144cb9afdb23961bcc892ee6f32d7c4d4e9
Caleb Tracey <caleb.tracey@gmail.com>
2021-05-01 21:00:38 -0400

    Part 10 finished. Needs tidying up

--- src/student/SearchByLyricsPhrase.java
+++ src/student/SearchByLyricsPhrase.java
@@ -1,15 +1,24 @@
 package student;
 
+import java.security.KeyStore.Entry;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SearchByLyricsPhrase {
 
     private static SearchByLyricsWords sblw;
     private TreeMap<String, TreeSet<Song>> lyricsWordMap = new TreeMap<>();
-    TreeMap<String, TreeMap<Integer, Song>> songsRanked = new TreeMap<>();
+    TreeMap<String, TreeMap<Song, Integer>> songsRanked = new TreeMap<>();
     String phrase;
 
     public SearchByLyricsPhrase(SongCollection sc) {
@@ -48,11 +57,11 @@ public class SearchByLyricsPhrase {
             if (rank != -1) {
                 String artist = song.getArtist();
                 if (!songsRanked.containsKey(artist)) {
-                    TreeMap<Integer, Song> newArtist = new TreeMap<>();
-                    newArtist.put(rank, song);
+                    TreeMap<Song, Integer> newArtist = new TreeMap<>();
+                    newArtist.put(song, rank);
                     songsRanked.put(artist, newArtist);
                 } else {
-                    songsRanked.get(artist).put(rank, song);
+                    songsRanked.get(artist).put(song, rank);
                 }
             }
         }
@@ -61,7 +70,7 @@ public class SearchByLyricsPhrase {
     public Song[] search(String lyricsPhrase) {
         String phraseReplace = lyricsPhrase.replace("\n", " ").replace("\r", " ").replaceAll("[^a-zA-Z ]", " ").trim()
                 .replaceAll("\\s+", " ");
-        Song[] rankedArray = new Song[0];
+        TreeMap<Integer, Song[]> rankedMap = new TreeMap<>();
         TreeSet<Song> lyricSongs = new TreeSet<>();
         TreeSet<String> searchSet = buildLyricsSet(phraseReplace);
         this.phrase = phraseReplace;
@@ -84,17 +93,32 @@ public class SearchByLyricsPhrase {
         }
 
         rankPhrase(wordSearch, phraseReplace);
+        // TreeMap<Integer, Song> mergeMap = new TreeMap<>();
+        ArrayList<Song> rankedSongs = new ArrayList<>();
 
         if (!songsRanked.isEmpty()) {
-
             for (String artist : songsRanked.keySet()) {
-                for (Integer rank : songsRanked.get(artist).keySet()) {
-                    rankedArray = Arrays.copyOf(rankedArray, rankedArray.length + 1);
-                    rankedArray[rankedArray.length + 1] = songsRanked.get(artist).get(rank);
+                TreeMap<Song, Integer> artistMap = new TreeMap<>(songsRanked.get(artist));
+                for (Song song : artistMap.keySet()) {
+                    int val = artistMap.get(song);
+                    if (!rankedMap.keySet().contains(val)) {
+                        Song[] newVal = new Song[1];
+                        newVal[0] = song;
+                        rankedMap.put(val, newVal);
+
+                    } else {
+                        Song[] newVal = Arrays.copyOf(rankedMap.get(val), rankedMap.get(val).length + 1);
+                        newVal[rankedMap.get(val).length] = song;
+                        rankedMap.put(val, newVal);
+                    }
                 }
             }
+            for (Integer rank : rankedMap.keySet()) {
+                rankedSongs.addAll(Arrays.asList(rankedMap.get(rank)));
+            }
         }
-        return rankedArray;
+
+        return rankedSongs.toArray(Song[]::new);
     }
 
     public static void main(String[] args) {
@@ -112,14 +136,12 @@ public class SearchByLyricsPhrase {
             System.out.printf("Search Terms: \"%s\"\n", args[1]);
             System.out.printf("Total Matches: %d\n", res.length);
             System.out.println("--------------------------------------------");
-            int printed = 0;
             for (int i = 0; i < 10; i++) {
-                System.out.printf("%d  %s", PhraseRanking.rankPhrase(res[i].getLyrics(), args[1]));
+            System.out.printf("%d %s\n", PhraseRanking.rankPhrase(res[i].getLyrics(),
+            args[1]), res[i]);
             }
             // System.out.println("------- ");
-            // Stream.of(res).limit(10).forEachOrdered(PhraseRanking.rankPhrase(t.getLyrics(),
-            // phrase));
-            // .forEach(System.out::println);
+            //Stream.of(res).limit(10).forEach(System.out::println);
 
         }
     }
